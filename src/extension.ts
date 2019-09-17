da86002d3c5beb4886ca917373d731cb3e48c110
Jan Bouwer <JBouwer@acm.org>
2019-09-18 00:33:59 +1000

    Miscellaneous & test-file cleanup.

--- src/extension.ts
+++ src/extension.ts
@@ -32,8 +32,6 @@ export function activate(context: vscode.ExtensionContext)
 
 export function deactivate(context: vscode.ExtensionContext)
 {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('"Boost Quickbook Support" extension is now deactivated.');
 }
 
